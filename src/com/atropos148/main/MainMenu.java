dc7893fd6deb5ce89a95668d2c279020c9f9f6bf
Atropos148 <tibor.h148@gmail.com>
2022-12-27 10:33:29 +0100

    Added Day4 Solution

--- src/com/atropos148/main/MainMenu.java
+++ src/com/atropos148/main/MainMenu.java
@@ -3,28 +3,33 @@ package com.atropos148.main;
 import com.atropos148.solutions.Day1;
 import com.atropos148.solutions.Day2;
 import com.atropos148.solutions.Day3;
+import com.atropos148.solutions.Day4;
 
 public class MainMenu {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		Day1 day1 = new Day1();
-		System.out.println("Day 1: Test: " + day1.getResultTest());
-		System.out.println("Day 1: Result: " + day1.getResult());
-		System.out.println("Day 1: Test TopThree: " + day1.getResultExtraTest());
-		System.out.println("Day 1: Result TopThree: " + day1.getResultExtra());
+		// Day1 day1 = new Day1();
+		// System.out.println("Day 1: Test: " + day1.getResultTest());
+		// System.out.println("Day 1: Result: " + day1.getResult());
+		// System.out.println("Day 1: Test TopThree: " + day1.getResultExtraTest());
+		// System.out.println("Day 1: Result TopThree: " + day1.getResultExtra());
 
-		Day2 day2 = new Day2();
-		System.out.println("Day 2 Test: " + day2.getResultTest());
-		System.out.println("Day 2 Result: " + day2.getResult());
-		System.out.println("Day 2 Extra Test: " + day2.getResultExtraTest());
-		System.out.println("Day 2 Extra Result: " + day2.getResultExtra());
+		// Day2 day2 = new Day2();
+		// System.out.println("Day 2 Test: " + day2.getResultTest());
+		// System.out.println("Day 2 Result: " + day2.getResult());
+		// System.out.println("Day 2 Extra Test: " + day2.getResultExtraTest());
+		// System.out.println("Day 2 Extra Result: " + day2.getResultExtra());
 
-		Day3 day3 = new Day3();
-		System.out.println("Day 3 Test: " + day3.getResultTest());
-		System.out.println("Day 3: " + day3.getResult());
-		System.out.println("Day 3 Extra Test: " + day3.getResultExtraTest());
-		System.out.println("Day 3 Extra: " + day3.getResultExtra());
+		// Day3 day3 = new Day3();
+		// System.out.println("Day 3 Test: " + day3.getResultTest());
+		// System.out.println("Day 3: " + day3.getResult());
+		// System.out.println("Day 3 Extra Test: " + day3.getResultExtraTest());
+		// System.out.println("Day 3 Extra: " + day3.getResultExtra());
+
+		Day4 day4 = new Day4();
+		System.out.println("Day 4 Test: " + day4.getResultTest());
+		System.out.println("Day 4: " + day4.getResult());
 	}
 
 }
---
+++ src/com/atropos148/solutions/Day4.java
@@ -0,0 +1,78 @@
+package com.atropos148.solutions;
+
+import com.atropos148.main.DataReader;
+import com.atropos148.main.Day;
+
+public class Day4 implements Day {
+
+    private String resultTest = "";
+    private String result = "";
+
+    private String resultTestExtra = "";
+    private String resultExtra = "";
+
+    private String testData = """
+            2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8""";
+
+    private String checkRanges(String ranges) {
+        int result = 0;
+        String[] allRanges = ranges.split("\n");
+        for (String range : allRanges) {
+            String firstRange = range.split(",")[0];
+            String secondRange = range.split(",")[1];
+
+            if (isRangeWithin(firstRange, secondRange)) {
+                result += 1;
+            }
+        }
+        return String.valueOf(result);
+    }
+
+    private boolean isRangeWithin(String firstRange, String secondRange) {
+        String[] firstRangeDetails = firstRange.split("-");
+        String[] secondRangeDetails = secondRange.split("-");
+
+        if (Integer.parseInt(firstRangeDetails[0]) <= Integer.parseInt(secondRangeDetails[0])) {
+            if (Integer.parseInt(firstRangeDetails[1]) >= Integer.parseInt(secondRangeDetails[1])) {
+                return true;
+            }
+        }
+
+        if (Integer.parseInt(secondRangeDetails[0]) <= Integer.parseInt(firstRangeDetails[0])) {
+            if (Integer.parseInt(secondRangeDetails[1]) >= Integer.parseInt(firstRangeDetails[1])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getResultTest() {
+        return checkRanges(testData);
+    }
+
+    @Override
+    public String getResult() {
+        String realData = DataReader.readData("Day4Data.txt");
+        return checkRanges(realData);
+    }
+
+    @Override
+    public String getResultExtraTest() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getResultExtra() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
