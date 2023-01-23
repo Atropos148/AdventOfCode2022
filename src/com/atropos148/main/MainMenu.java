a5019a7f80ef1c5129507c71857a26a4d809083a
Atropos148 <tibor.h148@gmail.com>
2023-01-23 14:13:10 +0100

    Added Day6 Test Solution

--- src/com/atropos148/main/MainMenu.java
+++ src/com/atropos148/main/MainMenu.java
@@ -5,39 +5,42 @@ import com.atropos148.solutions.Day2;
 import com.atropos148.solutions.Day3;
 import com.atropos148.solutions.Day4;
 import com.atropos148.solutions.Day5;
+import com.atropos148.solutions.Day6;
 
 public class MainMenu {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		Day1 day1 = new Day1();
+		Day day1 = new Day1();
 		System.out.println("Day 1: Test: " + day1.getResultTest());
 		System.out.println("Day 1: Result: " + day1.getResult());
 		System.out.println("Day 1: Test TopThree: " + day1.getResultExtraTest());
 		System.out.println("Day 1: Result TopThree: " + day1.getResultExtra());
 
-		Day2 day2 = new Day2();
+		Day day2 = new Day2();
 		System.out.println("Day 2 Test: " + day2.getResultTest());
 		System.out.println("Day 2 Result: " + day2.getResult());
 		System.out.println("Day 2 Extra Test: " + day2.getResultExtraTest());
 		System.out.println("Day 2 Extra Result: " + day2.getResultExtra());
 
-		Day3 day3 = new Day3();
+		Day day3 = new Day3();
 		System.out.println("Day 3 Test: " + day3.getResultTest());
 		System.out.println("Day 3: " + day3.getResult());
 		System.out.println("Day 3 Extra Test: " + day3.getResultExtraTest());
 		System.out.println("Day 3 Extra: " + day3.getResultExtra());
 
-		Day4 day4 = new Day4();
+		Day day4 = new Day4();
 		System.out.println("Day 4 Test: " + day4.getResultTest());
 		System.out.println("Day 4: " + day4.getResult());
 		System.out.println("Day 4 Extra Test: " + day4.getResultExtraTest());
 		System.out.println("Day 4 Extra: " + day4.getResultExtra());
 
-		Day5 day5 = new Day5();
+		Day day5 = new Day5();
 		// System.out.println("Day 5 Test: " + day5.getResultTest());
+		// System.out.println("Day 5: " + day5.getResult());
 
-		System.out.println("Day 5: " + day5.getResult());
+		Day day6 = new Day6();
+		System.out.println("Day 6 Test: " + day6.getResultTest());
 	}
 
 }
---
+++ src/com/atropos148/solutions/Day6.java
@@ -0,0 +1,52 @@
+package com.atropos148.solutions;
+
+import com.atropos148.main.Day;
+
+public class Day6 implements Day {
+    String testData = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+    private boolean areCharactersUnique(String characters) {
+        for (int i = 0; i < characters.length(); i++) {
+            Character currentLetter = characters.charAt(i);
+            if (characters.chars().filter(ch -> ch == currentLetter).count() > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int findPacketStart(String data) {
+        int currentIndex = 0;
+
+        for (currentIndex = 4; currentIndex < data.length(); currentIndex++) {
+            if (areCharactersUnique(data.substring(currentIndex - 4, currentIndex)) == true) {
+                return currentIndex;
+            }
+        }
+        return currentIndex;
+    }
+
+    @Override
+    public String getResultTest() {
+        return String.valueOf(findPacketStart(testData));
+    }
+
+    @Override
+    public String getResult() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getResultExtraTest() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getResultExtra() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
